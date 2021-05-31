4df2e9d61445b9537e227ee31b5b64a0522b10f5
gui-iar <61202321+gui-iar@users.noreply.github.com>
2021-05-31 11:48:38 -0300

    Update set_pps_dcd.sh

--- set_pps_dcd.sh
+++ set_pps_dcd.sh
@@ -1,5 +1,15 @@
 #bash
 # sudo apt-get install setserial ntp pps-tools
+#server tick.iar.unlp.edu.ar prefer minpoll 4 maxpoll 4
+#server ntp.aggo-conicet.gob.ar
+#server 127.127.22.0                   # ATOM(PPS)
+#fudge 127.127.22.0 flag3 1            # enable PPS API
+#server ntp-time.for.mydomain
+#server otherntp.server.org
+#server ntp.research.gov
+
+
+
 ntpd
 service ntp restart
 modprobe pps_ldisc
